9c73bccf3dda9b5137cafedc55a3cc833d0cd09a
Emmanuel Cruz <emmanuelpornel1@gmail.com>
2024-11-10 21:19:12 +0800

    homepage

---
+++ components/header.php
@@ -0,0 +1,19 @@
+<header>
+    <a href="/index.php" class="logosec">
+        <img class="logo" src="../public/images/logo.png" alt="">
+        <h3 class="logoname"></>CDM CAMPUS CART</h3>
+    </a>
+
+    <ul class="menu">
+        <li><a href="#">DEPARTMENT SECTION</a></li>
+        <li><a href="/controllers/products.php">PRODUCTS</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">CONTACT US</a></li>
+    </ul>
+
+    <div class="icons">
+        <img src="../public/icons/user.png" alt="">
+        <img src="../public/icons/cart.png" alt="">
+    </div>
+
+</header>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./public/styles/global.css">
+</head>
+<body>
+    <?php require("./components/header.php")?>
+    <section class="hero">
+        <div class="content">
+            <div class="contentBlock">
+                <h1>Welcome to <br> <span>CDM</span> Campus <br>Cart</h1>
+                <input type="text" placeholder="What are you looking for? ">
+                <button>LOG IN</button>
+
+            </div>
+            <div class="ill">
+                <img class="illu" src="./public/images/illustration.png" alt="">
+            </div>
+        </div>
+    </section>
+
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
